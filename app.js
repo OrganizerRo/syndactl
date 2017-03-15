d721e32e7c4fb11e3a07a0cfec5c7627116f5a56
Ro <jmf1jmf1@yahoo.com>
2017-03-15 13:01:35 -0700

    Working commit with styles

--- app.js
+++ app.js
@@ -3,18 +3,25 @@
 // Declare app level module which depends on views, and components
 var app = angular.module('qzApp', []);
 
+app.filter('pct', function () {
+    return function (value, nod) {
+        return value.toFixed(nod);
+    }
+});
+
 app.controller("quizController", ['$scope', function($scope){
     
     $scope.SequentialMode = false;
     $scope.StudyMode = true;
     $scope.NumOfChoices = 4;
-    
+    $scope.lastResult = {};
+	
     $scope.db = [
         {
             qid: 1,
             question: "What is you rname",
             answer: "jerome",
-            picture: "1.gif",
+            picture: "12Glyphs.gif",
             hint: "",
             study: ""
         },
@@ -22,7 +29,7 @@ app.controller("quizController", ['$scope', function($scope){
             qid: 2,
             question: "How do you do",
             answer: "fine",
-            picture: "2.gif",
+            picture: "12Glyphs.gif",
             hint: "",
             study: ""
         },
@@ -30,7 +37,7 @@ app.controller("quizController", ['$scope', function($scope){
             qid: 3,
             question: "Foot size",
             answer: "13",
-            picture: "3.gif",
+            picture: "12Glyphs.gif",
             hint: "",
             study: ""
         },
@@ -38,23 +45,27 @@ app.controller("quizController", ['$scope', function($scope){
             qid: 4,
             question: "Pant Width",
             answer: "34",
-            picture: "4.gif",
+            picture: "12Glyphs.gif",
             hint: "",
             study: ""
         },
         {
             qid: 5,
-            question: "How to process food",
-            answer: "with a machine",
-            picture: "4.gif",
+            question: "",
+            answer: "34",
+            picture: "12Glyphs.gif",
             hint: "",
             study: ""
         }
     ];
     $scope.Question = {};
-    $scope.lstAnswers = [
-        
-    ];    
+    $scope.lstAnswers = [];
+	$scope.lastResult = { 
+		correct: true,
+		msg: "Good Luck",
+		Question: null
+	};
+	
     $scope.ResponseMessage = "";
     
     $scope.CorrectAnswers = 0;
@@ -63,14 +74,13 @@ app.controller("quizController", ['$scope', function($scope){
     $scope.init = function(){
         $scope.LoadNextQuestion();
     }
-    
+    $scope.Score = function() {
+		if ($scope.CorrectAnswers + $scope.IncorrectAnswers == 0)
+			return 100;
+		return (($scope.CorrectAnswers / ($scope.CorrectAnswers + $scope.IncorrectAnswers)) * 100);
+	}
     $scope.Randomize =  function(start, end) {
         return Math.floor((Math.random() * end) + start);
-    };
-    
-     $scope.RandomizeFrom =  function(arrOfStuff) {
-         var aLen = arrOfStuff.length;
-         return arrOfStuff[Math.floor((Math.random() * aLen) + 1)];
     };
     
     $scope.QuestionsCount = function(){
@@ -78,21 +88,34 @@ app.controller("quizController", ['$scope', function($scope){
     };
     
     $scope.TestAnswer = function(qid){        
-        $scope.ResponseMessage =  (qid == $scope.Question.qid)? " Correct ! " : " In Correct";
-        $scope.LoadNextQuestion();
+        var isRight = (qid == $scope.Question.qid);
+        
+		$scope.lastResult = { 
+			correct: isRight,
+			msg: (isRight)? "Great Job !": "InCorrect",
+			Question: null,
+			cssClass:  (isRight)? "correct": "incorrect"
+		};
+		if(isRight) {
+			$scope.CorrectAnswers++;
+		} else {
+			$scope.IncorrectAnswers++;
+		}
+		$scope.LoadNextQuestion();
     };
     
     $scope.GetRandomQuestion = function(){
-        var rndQuestionIX = $scope.Randomize(0, $scope.db.length - 1);
+        var rndQuestionIX = $scope.Randomize(0, $scope.db.length -1);
         return $scope.db[rndQuestionIX];  //question row object
     };
     
     $scope.GetRandomAnswer = function(NotAllowedAnswers){
-        var allowedAnswers = $scope.db.filter(function(co, ix, ar){            
-            if(NotAllowedAnswers.length ==0)
-                return true;            
-            return !NotAllowedAnswers.find(function(ob,x,a){ return ob.qid == co.qid;});
+        var allowedAnswers = $scope.db.filter(function(co, ix, ar){
+            return NotAllowedAnswers.find(function(o,i,r){
+				return o.qid == co.qid;
+			}) == undefined;
         });
+				
         
         var rndQuestionIX = $scope.Randomize(0, allowedAnswers.length - 1);
         
@@ -103,30 +126,34 @@ app.controller("quizController", ['$scope', function($scope){
     $scope.LoadNextQuestion = function(){
         var lQuestion = $scope.GetRandomQuestion();
         $scope.Question = lQuestion;
-        
-        if($scope.Question == undefined) {
-            $scope.ResponseMessage = "Couldn't Load Question";
-            alert("stopped");
-        };
-        
-        $scope.lstAnswers = [];
-        
+        if(lQuestion == undefined){
+			console.log("Question was not found"); 
+		}
+		
+        $scope.lstAnswers = [{ 
+			qid: lQuestion.qid,
+			answer:lQuestion.answer
+		}];
+		
         var randomIX = $scope.Randomize(1, $scope.NumOfChoices);
         
-        for(var x=0;x < $scope.NumOfChoices;x++){
+        for(var x=0;x < $scope.NumOfChoices - 1;x++){
             var rndAns = $scope.GetRandomAnswer($scope.lstAnswers);
 			
             $scope.lstAnswers.push({ 
-                    qid: rndAns.qid,
-                    answer:rndAns.answer
-                });
+				qid: rndAns.qid,
+				answer:rndAns.answer
+			});
         };
+		
+		$scope.lstAnswers.splice(0,1);
+		console.log(randomIX);
+		$scope.lstAnswers.splice(randomIX - 1,0, { 
+			qid: lQuestion.qid,
+			answer: lQuestion.answer
+		});
     };
-    
-    $scope.PopulateAnswers = function(){
-        $scope.lstAnswers = [];                
-    };    
-    
+     
     $scope.init();
 }]);
 
